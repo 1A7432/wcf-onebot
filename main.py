0ceb9d12e373d55e1acad7dd32a704e88e818a6a
尤振宇 <dirac_c@icloud.com>
2025-01-13 17:16:58 +0800

    Login API path

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import uvicorn
-from wcf_onebot.config import config
-from wcf_onebot.server import app
+from wcf_onebot.server import run
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "wcf_onebot.server:app",
-        host=config.host,
-        port=config.port,
-        reload=True
-    )
+    run()
